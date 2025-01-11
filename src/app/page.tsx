5d4d8ca875181e78f134db49f2e904582a05ecf1
EthanHogan <ethan.hogan7@gmail.com>
2025-01-11 11:41:32 -0600

    fixes centering on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
-    <main className="flex flex-col justify-center gap-5 px-5">
-      <div className="container flex flex-col items-center justify-center py-16">
+    <main className="flex flex-col items-center gap-5 px-5">
+      <div className="container flex justify-center py-16">
         <h1 className="text-5xl font-extrabold tracking-tight text-primary sm:text-[5rem]">
           next<span className="text-blue-500">-</span>patterns
         </h1>
